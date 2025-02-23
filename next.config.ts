4c24c0d1acd256b712b826cd6d81f037592e3c25
alvhursa1 <alvaro.hurtado.s@me.com>
2025-02-23 14:16:29 -0500

    front v1.5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['3c5d9cf7fdb3020abd9872db23eced33.r2.cloudflarestorage.com'],
+  },
 };
 
 export default nextConfig;
